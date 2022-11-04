5da6e75b4c762fcbf41030d9ab38b6a212dfcae9
Andrew Lanning <116489166+andrewlanning@users.noreply.github.com>
2022-11-04 01:18:11 -0500

    Fixed MORE Issues After Midnight

--- utils.py
+++ utils.py
@@ -10,8 +10,11 @@ def find_log_file(server_log_path):
     while True:
         ftype = r'\*log'
         files = glob.glob(server_log_path+ftype)
-        if str("output"+str(time.strftime("%Y%m%d_%-H%M%S"))+".log") == str(os.path.basename(max(files, key=os.path.getctime))):
+        if str("output"+time.strftime("%Y%m%d"+"_"+"%#H%M%S")+".log") == str(os.path.basename(max(files, key=os.path.getctime))):
             return max(files, key=os.path.getctime)
+        elif str(time.strftime("%H")) == str('00'):
+            if str("output"+time.strftime("%Y%m%d"+"_"+"%M%S")+".log") == str(os.path.basename(max(files, key=os.path.getctime))):
+                return max(files, key=os.path.getctime)
         else:
             pass
                   
