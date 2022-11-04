ce153d34fd0695ae7349d0708767cc249d9aca78
Andrew Lanning <116489166+andrewlanning@users.noreply.github.com>
2022-11-03 22:57:10 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ server_log_path = "E:\\SteamLibrary\\steamapps\\common\\assettocorsa\\server\\lo
 
 utils.find_log_file(server_log_path)
 
-newest_file = open(utils.find_log_file(server_log_path))
-
-for l in utils.follow(newest_file):
+for l in utils.follow(open(utils.find_log_file(server_log_path))):
     print(l)
