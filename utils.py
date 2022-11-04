bee9381746bd7150c389339bd8220896b64e8fef
Andrew Lanning <116489166+andrewlanning@users.noreply.github.com>
2022-11-04 00:17:36 -0500

    Bugfix to timestamp
    Fixed a bug where after midnight the script would stop functioning due to the hour being output as a decimal...

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def find_log_file(server_log_path):
     while True:
         ftype = r'\*log'
         files = glob.glob(server_log_path+ftype)
-        if str("output"+str(time.strftime("%Y%m%d_%H%M%S"))+".log") == str(os.path.basename(max(files, key=os.path.getctime))):
+        if str("output"+str(time.strftime("%Y%m%d_%-H%M%S"))+".log") == str(os.path.basename(max(files, key=os.path.getctime))):
             return max(files, key=os.path.getctime)
         else:
             pass
